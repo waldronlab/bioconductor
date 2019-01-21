394fd079fc50957072c8e4f205f41d44aa3206df
Levi Waldron <lwaldron.research@gmail.com>
2019-01-21 12:59:55 +0100

    remove libx11-dev, put commented packages at end

--- install.sh
+++ install.sh
@@ -7,12 +7,7 @@ apt-get install -y libpng-dev libnetcdf-dev libhdf5-serial-dev \
     sqlite libfftw3-dev libfftw3-doc libopenbabel-dev libfftw3-3 libfftw3-dev\
     pkg-config xfonts-100dpi xfonts-75dpi libopenmpi-dev openmpi-bin \
     python-mpi4py mpi-default-bin openmpi-common libexempi3 openmpi-doc \
-    texlive-fonts-recommended texlive-science texlive-bibtex-extra texlive-fonts-extra \
-    texlive-lang-european texinfo texlive-latex-extra \
-    texlive-pstricks texlive-pstricks-doc texlive-luatex \
-#    postgresql-server-dev-all \
-#    python-numpy python-pandas python-h5py \
-    libx11-dev libxt-dev apache2 \
+    libxt-dev apache2 \
     libjpeg62-turbo-dev libcairo2-dev  libtiff5-dev fortran77-compiler libreadline-dev \
     xvfb tcl8.5-dev tk8.5-dev libgsl2 libgsl0-dev libgtk2.0-dev \
     openjdk-8-jdk \
@@ -26,3 +21,9 @@ apt-get install -y libpng-dev libnetcdf-dev libhdf5-serial-dev \
     libudunits2-dev tree python-yaml libmodule-build-perl biber \
     libprotobuf-dev protobuf-compiler libapparmor-dev libgeos-dev \
     libprotoc-dev  librdf0-dev libmagick++-dev libsasl2-dev
+
+#    texlive-fonts-recommended texlive-science texlive-bibtex-extra texlive-fonts-extra \
+#    texlive-lang-european texinfo texlive-latex-extra \
+#    texlive-pstricks texlive-pstricks-doc texlive-luatex \
+#    postgresql-server-dev-all \
+#    python-numpy python-pandas python-h5py \
